b179d2e9d459a1488f785c2a26059605b69ddfbe
CharlesChG270612 <126025362+CharlesChG270612@users.noreply.github.com>
2025-09-10 18:29:24 -0600

    PracticaVideo

---
+++ JSParaPrincipiantes/app.js
@@ -0,0 +1,8 @@
+// Hello world en JavaScript
+//console.log("Hello world");
+//VM106:1 Hello world
+//document.write("Hello world") para mostrar en el documento sin ser html
+//console.error("Mi error customizado")
+
+
+//document.write("<h1>Hello world</h1>");
