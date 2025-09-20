a7847272c6efe1b4149db7e2026832dfa9f2d279
CharlesChG270612 <126025362+CharlesChG270612@users.noreply.github.com>
2025-09-19 18:13:51 -0600

    Ejercicio2

---
+++ Practica2/FuncionMapArreglo.js
@@ -0,0 +1,16 @@
+const productos = [
+    {nombre: "Laptop", precio: 12000},
+    {nombre: "Mouse", precio: 250},
+    {nombre: "Teclado", precio: 750},
+    {nombre: "Monitor", precio: 3000}
+];
+
+const NombreProductos = productos.filter(Producto =>Producto.precio > 1000).map(productos=>productos.nombre);
+console.log(NombreProductos);
+
+/*const NombreProductos = productos.map(Producto =>{
+if(Producto.precio > 1000){
+    return Producto.nombre;
+}
+});
+console.log(NombreProductos); */
