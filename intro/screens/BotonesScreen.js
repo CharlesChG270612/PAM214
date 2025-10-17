796939d0026ad13f311f54776254313b203283a6
CharlesChG270612 <126025362+CharlesChG270612@users.noreply.github.com>
2025-10-17 15:31:03 -0600

    Práctica No.8: Buttons & Switch

--- intro/screens/BotonesScreen.js
+++ intro/screens/BotonesScreen.js
@@ -1,13 +1,87 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Pressable, TouchableOpacity,TouchableWithoutFeedback, Switch } from 'react-native'
+import { useState } from 'react';
 
 
 export default function BotonesScreen() {
+   const [accion , setAction] = useState('nada');
+   const[isDisabled,setIsDisabled] = useState(false);
     return (
-      <View>
-        <Text>BotonesScreen</Text>
+      <View style={styles.container}>
+        <Text>{ accion }</Text>
+
+        <Text>Activado: </Text>
+        <Switch
+          onValueChange={() => setIsDisabled(!isDisabled)}
+          value={!isDisabled}
+        />
+
+
+        <Pressable onPressIn={()=>setAction('Pressable In')}
+          onPressOut={()=>setAction('Pressable Out')}
+          onLongPress={()=>setAction('Pressable Long')}
+          disabled={!isDisabled}>
+            {({pressed})=>(
+                <View style={[styles.card,!pressed && styles.shadow]}>
+                <View style={[styles.mockImage]}></View>
+                <Text>{pressed? 'Tarjeta presionada' :'Tarjeta no presionada'}</Text>
+                </View>
+            )}
+        </Pressable>
+
+
+        <TouchableOpacity activeOpacity={0.2}
+        onPress={()=>setAction('Opacity')}
+        disabled={!isDisabled}>
+          <View style={[styles.card, styles.shadow]}>
+          <View style={[styles.mockImage]}></View>  
+          <Text>Esta es una tarjeta</Text>
+          </View>
+        </TouchableOpacity>
+
+
+        <TouchableWithoutFeedback
+        onPress={()=>setAction('Whitout Feedback')}
+        disabled={!isDisabled}>
+          <View style={[styles.card, styles.shadow]}>
+          <View style={[styles.mockImage]}></View>  
+          <Text>Esta es una tarjeta</Text>
+          </View>
+        </TouchableWithoutFeedback>
+
+
       </View>
+
     )
   
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    card: {
+        width: 200,
+        height: 250,
+        padding: 20,
+        display: "flex",
+        flexDirection: 'column',
+        borderRadius: 8,
+        margin: 15
+    },
+    mockImage: {
+        flex: 1,
+        backgroundColor: 'gray',
+        marginBottom: 8,
+        borderRadius: 8
+    },
+    shadow: {
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 10},
+        shadowOpacity: 0.2,
+        shadowRadius: 12,
+        elevation: 10,
+    },
+});
