b34d96af10c50b83c6c674a73c79b0781ed9875b
CharlesChG270612 <126025362+CharlesChG270612@users.noreply.github.com>
2025-10-23 22:53:15 -0600

    Practica No.9: TextInput & Alert

--- intro/screens/TextScreen.js
+++ intro/screens/TextScreen.js
@@ -1,11 +1,82 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, TextInput, Button, Alert } from 'react-native'
+import React, {Component, useState} from 'react';
 
 export default function TextScreen() {
+const [nombre,setNombre] = useState('');
+const [contraseña,setContraseña] = useState('');
+const [multilinea,setMultilinea] = useState('');
+
+const MostrarAlerta = () => {
+  if([nombre.trim(),contraseña.trim(),multilinea.trim()].includes('')){
+    alert('Error, Por favor rellena todos los campos.');
+    Alert.alert('Error','Por favor rellena todos los campos.');
+  }else{
+    alert(`Hola,${nombre}!, Tu nombre ha sido registrado con éxito. Tu contraseña es ${contraseña}`);
+    Alert.alert(`Hola,${nombre}!, Tu nombre ha sido registrado con éxito. Tu contraseña es ${contraseña}`);
+    setNombre('');
+  }
+  
+};
     return (
-      <View>
-        <Text>TextScreen</Text>
+      <View style={styles.container} >
+        <Text style={styles.label}>Ingresa tu nombre:</Text>
+        <TextInput
+        style={styles.input}
+        placeholder='Ej. Carlos'
+        value={nombre}
+        onChangeText={setNombre}
+        keyboardType='default'
+        autoCapitalize='words'
+        />
+
+        <TextInput
+        style={styles.input}
+        placeholder='Ej. Password'
+        value={contraseña}
+        onChangeText={setContraseña}
+        keyboardType='numeric'
+        secureTextEntry = {true}
+        />
+
+        <TextInput
+        style={styles.input} 
+        multiline={true}
+        value={multilinea}
+        onChangeText={setMultilinea}
+        numberOfLines={4}
+        />
+
+        <Button
+        title='Saludar'
+        onPress={MostrarAlerta}
+        color= "pink"
+        />
+
       </View>
     )
 }
 
-const styles = StyleSheet.create({})
+// 4. Estilos para nuestros componentes
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#f5f5f5',
+  },
+  label: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  input: {
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 1, //Sirve para ver el borde del input
+    borderRadius: 8, // Bordes redondeados
+    paddingHorizontal: 15, // Espacio interno a los lados
+    marginBottom: 20, // Espacio debajo del input
+    fontSize: 16,
+  },
+});
