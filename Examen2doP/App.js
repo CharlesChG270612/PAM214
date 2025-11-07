db8db5990761785fc94240972fcc671229f4c224
CharlesChG270612 <126025362+CharlesChG270612@users.noreply.github.com>
2025-11-07 15:31:14 -0600

    Commit FInal

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet, View, SafeAreaView, ImageBackground } from 'react-native'
+import { Text, StyleSheet, View, SafeAreaView, ImageBackground,Button,Alert,ScrollView,RefreshControl } from 'react-native'
 import {useEffect, useState} from 'react';
 
 export default function App() {
@@ -10,6 +10,12 @@ export default function App() {
     return () => clearTimeout(timer);
   },[]);
 
+  const MostrarAlerta = () => {
+    alert('Guardar',);
+    Alert.alert();
+    
+};
+
   if(showsplash){
     return (
       <View style={styles.splashContainer}>
@@ -17,8 +23,13 @@ export default function App() {
       </View>
     );
   }
+
+
+ 
   return (
    <SafeAreaView style={{flex:1}}>
+      <ScrollView
+      >
       <ImageBackground 
       source={{
         uri: 'https://e0.pxfuel.com/wallpapers/39/219/desktop-wallpaper-black-dark-abstract.jpg',
@@ -26,15 +37,31 @@ export default function App() {
       style={styles.background}
       >
       <View style={styles.overlay}>
-        <Text style={styles.label}>Perfil personal</Text>
-        <Text style={styles.label}>Nombre completo: Carlos Chávez García</Text>
-        <Text style={styles.label}>Profesion/Título: Ingeniería en Tecnología de la Información e Innovación dígital</Text>
+        <Text style={styles.titulo}>Perfil personal</Text>
+        <Text style={styles.label}>Nombre completo:</Text>
+        <Text style={styles.content}>Carlos Chávez García</Text>
+        <Text style={styles.label}>Profesion/Título:</Text>
+        <Text style={styles.content}>Ingeniería en Tecnología de la Información e Innovación dígital</Text>
         <Text style={styles.label}>Breve biografía:</Text>
+        <Text style={styles.content}>Mi nombre es Carlos Chávez García, nací el 27 de Diciembre de 2006,</Text>
+        <Text style={styles.content}>vivo en Querétaro y estudio la Ingeniería en Tecnologías de Información e Innovación</Text>
+        <Text style={styles.content}>dígital en la Universidad Politecnica de Querétaro donde me encuentro cursando el</Text>
+        <Text style={styles.content}>cuarto cuatrimestre de la carrera, me interesa el manejo de software y las</Text>
+         <Text style={styles.content}>matemáticas donde principalmente destaco.</Text>
         <Text style={styles.label}>Correo electrónico:</Text>
+        <Text style={styles.content}>carlchavz43@gmail.com</Text>
         <Text style={styles.label}>Número de teléfono:</Text>
+        <Text style={styles.content}>+52 448 111 7319</Text>
+         <Button
+        title='Editar perfil'
+        onPress={MostrarAlerta}
+        color= "green"
+        />
       </View>
       </ImageBackground>
+      </ScrollView>
     </SafeAreaView>
+ 
   );
 }
 
@@ -63,10 +90,23 @@ const styles = StyleSheet.create({
     fontSize: 24, 
     color: '#fff', 
   },
-  label: {
-    fontSize: 20,
+   titulo: {
+    fontSize: 50,
     fontWeight: 'bold',
     marginBottom: 10,
+    textAlign: 'center',
+    color: '#fff',
+  },
+  label: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+    color: '#fff',
+  },
+    content: {
+    fontSize: 25,
+    marginBottom: 5,
     textAlign: 'center',
     color: '#fff',
   },
